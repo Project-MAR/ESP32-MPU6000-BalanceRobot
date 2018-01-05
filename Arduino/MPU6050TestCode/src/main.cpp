8f66c84d21d5b35389d5855ea871e106d921c8a8
Project-MAR <atitep.ram@gmail.com>
2018-01-06 00:01:20 +0700

    clean code in MPU6050TestCode

--- Arduino/MPU6050TestCode/src/main.cpp
+++ Arduino/MPU6050TestCode/src/main.cpp
@@ -2,19 +2,19 @@
 #include <Wire.h>
 
 // Original Code From
+// https://www.controleverything.com/content/Accelorometer?sku=MPU-6000_I2CS#tabs-0-product_tabset-2
 // Distributed with a free-will license.
 // Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
-// MPU-6000
-// This code is designed to work with the MPU-6000_I2CS I2C Mini Module available from ControlEverything.com.
-// https://www.controleverything.com/content/Accelorometer?sku=MPU-6000_I2CS#tabs-0-product_tabset-2
+// MPU-60x0
 
-// MPU-6000 I2C address is 0x68 if AD = 0, otherwise 0x69
+
+// For MPU-6000, I2C address is 0x68 When AD = 0, otherwise 0x69
 #define Addr 0x68
 #define LED_BUILTIN 2
 
-#define MPU_RAW      0  // 2's Complement format but print like decimal format
-#define MPU_DECIMAL  1  // 2's Complement format that convert to decimal format
-#define MPU_REALDATA 2  // decimal format that convert to actual data
+#define MPU_RAW        0    // 2's Complement format but print like decimal format
+#define MPU_DECIMAL    1    // 2's Complement format that convert to decimal format
+#define MPU_REALDATA   2    // decimal format that convert to actual data
 
 unsigned int data[6];
 int xAccl  = 0;
@@ -24,13 +24,12 @@ int xGyro  = 0;
 int yGyro  = 0;
 int zGyro  = 0;
 
+// Original Code From http://forum.arduino.cc/index.php/topic,44216.0.html#13
+// prints val with number of decimal places determine by precision
+// NOTE: precision is 1 followed by the number of zeros for the desired number of decimial places
+// example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
 void printDouble(double val, unsigned int precision)
 {
-  // Original Code From http://forum.arduino.cc/index.php/topic,44216.0.html#13
-  // prints val with number of decimal places determine by precision
-  // NOTE: precision is 1 followed by the number of zeros for the desired number of decimial places
-  // example: printDouble( 3.1415, 100); // prints 3.14 (two decimal places)
-
    Serial.print (int(val));   //prints the int part
    Serial.print(".");         // print the decimal point
    unsigned int frac;
@@ -52,37 +51,29 @@ void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
 
-  Serial.begin(115200);           // Initialise serial communication, set baud rate = 115200
+  Serial.begin(115200);             // Initialise serial communication, set baud rate = 115200
   
-  Wire.begin();           // Initialise I2C communication as Master
-  Wire.setClock(400000);  // choose 400 kHz I2C rate
+  Wire.begin();                     // Initialise I2C communication as Master
+  Wire.setClock(400000);            // choose 400 kHz I2C rate
 
-  Wire.beginTransmission(Addr);   // Start I2C transmission
-  Wire.write(0x1B);               // Select gyroscope configuration register
+  Wire.beginTransmission(Addr);     // Start I2C transmission
+  Wire.write(0x1B);                 // Select gyroscope configuration register
   //Wire.write(0x18);               // Full scale range = +/-2000 dps
   //Wire.write(0x08);               // Full scale range = +/-500 dps
-  Wire.write(0x00);             // Scale range = +/-250 dps
-  Wire.endTransmission();         // Stop I2C transmission
+  Wire.write(0x00);                 // Scale range = +/-250 dps
+  Wire.endTransmission();           // Stop I2C transmission
 
-  Wire.beginTransmission(Addr);   // Start I2C transmission
-  Wire.write(0x1C);               // Select accelerometer configuration register
+  Wire.beginTransmission(Addr);     // Start I2C transmission
+  Wire.write(0x1C);                 // Select accelerometer configuration register
   //Wire.write(0x18);               // Full scale range = +/-16g
   //Wire.write(0x08);               // Full scale range = +/-4g
-  Wire.write(0x00);             // Scale range = +/-2g
-  Wire.endTransmission();         // Stop I2C transmission
+  Wire.write(0x00);                 // Scale range = +/-2g
+  Wire.endTransmission();           // Stop I2C transmission
 
-  Wire.beginTransmission(Addr);   // Start I2C transmission
-  Wire.write(0x6B);               // Select power management register
-  Wire.write(0x01);               // PLL with xGyro reference
-  Wire.endTransmission();         // Stop I2C transmission
-
-  /*
-  Serial.print("AccStepCoeff: ");
-  printDouble(AccStepCoeff, 10000000);
-  Serial.print("GyroStepCoeff: ");
-  printDouble(GyroStepCoeff, 10000000);
-  while(1){}
-  */
+  Wire.beginTransmission(Addr);     // Start I2C transmission
+  Wire.write(0x6B);                 // Select power management register
+  Wire.write(0x01);                 // PLL with xGyro reference
+  Wire.endTransmission();           // Stop I2C transmission
   delay(500);
 }
 
@@ -90,12 +81,12 @@ int TwoComplementToNormalValue(int data)
 {
   unsigned char raw[2];
   int result = 0;
-  /*
-  Serial.print("2's complement value: ");
-  Serial.println(data);
-  Serial.print("minus check: ");
-  Serial.println(data & 0x8000);
-  */
+  
+  //Serial.print("2's complement value: ");
+  //Serial.println(data);
+  //Serial.print("minus check: ");
+  //Serial.println(data & 0x8000);
+
   if((data & 0x8000) != 0)
   {
     //Serial.println("Detect Negative Value");
@@ -114,13 +105,11 @@ int TwoComplementToNormalValue(int data)
     //Serial.println("Detect Positive Value");
     result = data;
   }
-  /*
-  Serial.print("Decimal Value: ");
-  Serial.println(result);
   
-  while(true)
-  { }
-  */
+  //Serial.print("Decimal Value: ");
+  //Serial.println(result);
+  //while(true){ };
+  
   return result;
 }
 
@@ -160,6 +149,7 @@ void printMPU6000(int mode)
   }
   else if(mode == MPU_REALDATA)
   {
+    /* Not Working, DO NOT USE */
     Serial.println("");
  
     float Real_Accl_X = (((32768.0 + (float)TwoComplementToNormalValue(xAccl)) * 4.0) / 65535.0) - 2.0;
@@ -177,7 +167,7 @@ void printMPU6000(int mode)
     Serial.print(TwoComplementToNormalValue(xAccl));
     Serial.print(" => ");
     printDouble(Real_Accl_X, 10000000);
-/*
+    /*
     Serial.print("Acceleration in X-Axis : ");
     printDouble(TwoComplementToNormalValue(zGyro), 10000000);
     Serial.print("Acceleration in Y-Axis : ");
@@ -209,7 +199,6 @@ void loop()
     status = 1;
     digitalWrite(LED_BUILTIN, LOW);
   }
-  
   
   Wire.beginTransmission(Addr);   // Start I2C transmission
   Wire.write(0x3B);               // Select data register
